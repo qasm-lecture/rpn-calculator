53448ed3f06f0e3dfd5db859feff05c10a12a7a4
trimaa <lennart.piepel@gmx.de>
2023-01-28 14:58:08 +0000

    disabled fuzzing ui and fixing build

--- fuzzing.sh
+++ fuzzing.sh
@@ -1,20 +1,18 @@
 export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
+export AFL_NO_UI=1
 
-timeout --preserve-status 5m make fuzz
+timeout --preserve-status 10s make fuzz
 
-if [[ "$(ls -A ./out/hangs)"]] || [["$(ls -A ./out/crashes)" ]]
-    then 
-        if [ "$(ls -A ./out/hangs)"]
-            then
-                echo "hangs"
-                find ./out/hangs -maxdepth 1 -type f -not -name 'README.txt' -exec cat {} \;
-        fi
+if ! [ -z "$(ls -A ./out/hangs)" ] || ! [ -z "$(ls -A ./out/crashes)" ]; then   
+    if ! [ -z "$(ls -A ./out/hangs)" ]; then
+        echo "hangs"
+        find ./out/hangs -maxdepth 1 -type f -not -name 'README.txt' -exec cat {} \;
+    fi
 
-        if [ "$(ls -A ./out/crashes)" ]
-            then
-                echo "chrashes"
-                find ./out/crashes  -maxdepth 1 -type f -not -name 'README.txt' -exec cat {} \;
-        fi
+    if ! [ -z "$(ls -A ./out/crashes)" ]; then
+        echo "crashes"
+        find ./out/crashes -maxdepth 1 -type f -not -name 'README.txt' -exec cat {} \;
+    fi
 
-        exit 1
+    exit 1
 fi
