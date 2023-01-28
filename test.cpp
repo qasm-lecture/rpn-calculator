bc7cb9442646095779df3f5aa36342b55754eada
Stefan Arnold <sa206710@fh-muenster.de>
2023-01-28 10:17:01 +0100

    Fixed tests for invalid calls and added test execution to pipeline

--- test.cpp
+++ test.cpp
@@ -8,32 +8,33 @@ TEST(Evaluate, simple_addition_works) {
     EXPECT_EQ(10, evaluate("19+"));
 }
 TEST(Evaluate, simple_substraction_works){
-EXPECT_EQ(0, evaluate("00-"));
-EXPECT_EQ(0, evaluate("11-"));
-EXPECT_EQ(6, evaluate("93-"));
-EXPECT_EQ(-6, evaluate("39-"));
+    EXPECT_EQ(0, evaluate("00-"));
+    EXPECT_EQ(0, evaluate("11-"));
+    EXPECT_EQ(6, evaluate("93-"));
+    EXPECT_EQ(-6, evaluate("39-"));
 }
 
 TEST(Evaluate, simple_divide_works){
-EXPECT_EQ(1, evaluate("11/"));
-EXPECT_EQ(3, evaluate("93/"));
-EXPECT_EQ(0, evaluate("39/"));
+    EXPECT_EQ(1, evaluate("11/"));
+    EXPECT_EQ(3, evaluate("93/"));
+    EXPECT_EQ(0, evaluate("39/"));
 }
 
 TEST(Evaluate, simple_divide_fails){
-EXPECT_ANY_THROW(evaluate("00/"));
+    EXPECT_DEATH(evaluate("00/"), "");
 }
 
 TEST(Evaluate, simple_multiply_works){
     EXPECT_EQ(0, evaluate("10*"));
-EXPECT_EQ(1, evaluate("11*"));
-EXPECT_EQ(81, evaluate("99*"));
+    EXPECT_EQ(1, evaluate("11*"));
+    EXPECT_EQ(81, evaluate("99*"));
 }
+
 TEST(Evaluate, invalid_input_fails){
-EXPECT_ANY_THROW(evaluate("-"));
-EXPECT_ANY_THROW(evaluate("1*"));
-EXPECT_ANY_THROW(evaluate("blub"));
-EXPECT_ANY_THROW(evaluate("b/"));
+    EXPECT_DEATH(evaluate("-"), "");
+    EXPECT_DEATH(evaluate("1*"), "");
+    EXPECT_DEATH(evaluate("blub"), "");
+    EXPECT_DEATH(evaluate("b/"), "");
 }
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
